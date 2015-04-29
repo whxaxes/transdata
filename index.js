94681b531ce9f225fca450b5a03646820eae0792
王鸿兴 <851094531@qq.com>
2015-04-29 15:32:05 +0800

    update

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ var transdata = {
     },
 
     get: function (opt) {
-        if(arguments.length>=2 && (typeof arguments[0]=="string") && (typeof arguments[1]=="function")){
+        if (arguments.length >= 2 && (typeof arguments[0] == "string") && (typeof arguments[1] == "function")) {
             opt = {
-                url:arguments[0],
-                success:arguments[1]
+                url: arguments[0],
+                success: arguments[1]
             };
 
-            if(arguments[2] && (typeof arguments[2]=="function")){
+            if (arguments[2] && (typeof arguments[2] == "function")) {
                 opt.error = arguments[2];
             }
         }
@@ -35,10 +35,8 @@ var transdata = {
 function main(opt) {
     var options, creq;
 
-//    可以为response对象，也可以为一个可写流
+//    res可以为response对象，也可以为一个可写流，success和error为请求成功或失败后的回调
     opt.res = ((opt.res instanceof http.ServerResponse) || (opt.res instanceof stream.Writable)) ? opt.res : null;
-
-//    请求成功或失败后的回调
     opt.success = (typeof opt.success == "function") ? opt.success : noop;
     opt.error = (typeof opt.error == "function") ? opt.error : noop;
 
@@ -53,56 +51,50 @@ function main(opt) {
         return;
     }
 
-    if (opt.method == "get") {
-        http.get(opt.url.href, function (res) {
-            reqCallback(opt.res, res, opt.success)
-        }).on("error", function (e) {
-            opt.error(e);
-        })
-    } else {
-        options = {
-            hostname: opt.url.hostname,
-            port: opt.url.port,
-            path: opt.url.pathname,
-            method: "post"
-        };
+    options = {
+        hostname: opt.url.hostname,
+        port: opt.url.port,
+        path: opt.url.pathname,
+        method: opt.method,
+        headers: {
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.37 Safari/537.36'
+        }
+    };
 
-//        如果req为可读流则使用pipe连接，传输数据，如果不是则直接write
+//  如果req为可读流则使用pipe连接，传输数据，如果不是则直接写出字符串
+    if (opt.method == 'post') {
         if (opt.req instanceof stream.Readable) {
-            if ('headers' in opt.req) {
-                options['headers'] = {
-                    'Content-Type': opt.req.headers['content-type'],
-                    'Content-Length': opt.req.headers['content-length']
-                };
+            if(opt.req instanceof http.IncomingMessage){
+                options.headers["Content-Type"] = opt.req.headers["content-type"];
+                options.headers["Content-Length"] = opt.req.headers["content-length"];
             }
-
-            process.nextTick(function(){
+            process.nextTick(function () {
                 opt.req.pipe(creq);
             })
-        } else if ((typeof opt.req) == "string") {
-            options['headers'] = {
-                'Content-Type': "text/plain; charset=utf-8",
-                'Content-Length': opt.req.length
-            };
+        } else {
+            var str = ((typeof opt.req) == "string") ? opt.req : "";
+            options.headers["Content-Length"] = str ? 0 : (new Buffer(str)).length;
 
-            process.nextTick(function(){
-                creq.write(opt.req);
-                creq.end();
+            process.nextTick(function () {
+                creq.end(str);
             })
-        }else {
-            opt.error(new Error("illegal request"));
-            return;
         }
-
-        creq = http.request(options, function (res) {
-            reqCallback(opt.res, res, opt.success)
-        }).on('error', function (e) {
-            opt.error(e);
+    } else {
+        process.nextTick(function () {
+            creq.end();
         })
     }
+
+    creq = http.request(options, function (res) {
+        reqCallback(opt.res, res, opt.success)
+    }).on('error', function (e) {
+        opt.error(e);
+    });
 }
 
-//请求的回调
+//请求成功后的回调
 function reqCallback(ores, res, callback) {
     if (ores) {
         ores.on('finish', function () {
@@ -112,9 +104,9 @@ function reqCallback(ores, res, callback) {
         if (ores instanceof http.ServerResponse) {
             var options = {};
 
-            //复制相应头信息
-            if(res.headers){
-                for(var k in res.headers){
+            //复制响应头信息
+            if (res.headers) {
+                for (var k in res.headers) {
                     options[k] = res.headers[k];
                 }
             }
@@ -134,16 +126,16 @@ function reqCallback(ores, res, callback) {
             var buffer = Buffer.concat(chunks, size);
 
             //如果数据用gzip或者deflate压缩，则用zlib进行解压缩
-            if(res.headers && res.headers['content-encoding'] && res.headers['content-encoding'].match(/(\bdeflate\b)|(\bgzip\b)/)){
-                zlib.unzip(buffer , function(err , buffer){
-                    if(!err){
+            if (res.headers && res.headers['content-encoding'] && res.headers['content-encoding'].match(/(\bdeflate\b)|(\bgzip\b)/)) {
+                zlib.unzip(buffer, function (err, buffer) {
+                    if (!err) {
                         callback(buffer.toString())
-                    }else {
+                    } else {
                         console.log(err);
                         callback("");
                     }
                 });
-            }else {
+            } else {
                 callback(buffer.toString())
             }
         })
@@ -151,4 +143,3 @@ function reqCallback(ores, res, callback) {
 }
 
 module.exports = transdata;
-
